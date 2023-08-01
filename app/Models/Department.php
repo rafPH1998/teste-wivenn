1e24cc25a68c9b22b7f69f4d9f7cf0b3045a2a67
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-08-01 16:50:56 -0300

    criando migrations e relacionamentos de: - departments (departamentos) - employees (funcionarios) - tasks (tarefas)

---
+++ app/Models/Department.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Department extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+}
